facdc7b82e1cd3fa634d608ce03ca0b58d05a902
Kaihaan <kaihaan@gmail.com>
2017-08-08 12:14:21 +0100

    partial

--- wp-content/themes/amuse/content-grid.php
+++ wp-content/themes/amuse/content-grid.php
@@ -1,22 +1,19 @@
 <!-- horizontal rule -->
 <hr>
 
-<!-- Post Thumbnail with defaukts stored in /uploads/category-images/-->
+<!-- Post Thumbnail -->
 
 <?php if ( has_post_thumbnail() ) : ?>
+	<!-- post's own thumbnail -->
+	<div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large',  array( 'class' => 'img-responsive center-block' )); ?></a></div>
 
-<div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large',  array( 'class' => 'img-responsive center-block' )); ?></a></div>
-
-<?php else : 
-$category = get_the_category(); 
-?>
-
-<div class="entry-thumbnail">
-<a href="<?php the_permalink(); ?>"><img class="img-responsive center-block" src="<?php bloginfo('url'); ?>/wp-content/uploads/category-images/<?php echo $category[0]->category_nicename ; ?>-360x180.jpg" alt="<?php the_title(); ?>" /></a>
-</div>
+<?php else : $category = get_the_category(); ?>
+	<!-- defaukt category thumbnail -->
+	<div>
+		<a href="<?php the_permalink(); ?>"><img class="img-responsive center-block" src="<?php bloginfo('url'); ?>/wp-content/uploads/category-images/<?php echo $category[0]->category_nicename ; ?>-360x180.jpg" alt="<?php the_title(); ?>" /></a>
+	</div>
 
 <?php endif; ?>
-
 
 <!-- Post Title -->
 <div class='grid-title'><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></div>
