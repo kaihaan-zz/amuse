6c459a91b64dcfc2e5454f2f3ff1057a63bd85ed
Kaihaan Jamshidi <kaihaan@gmail.com>
2017-08-08 20:14:32 +0100

    partial merge

--- wp-content/themes/amuse/content-grid.php
+++ wp-content/themes/amuse/content-grid.php
@@ -7,23 +7,11 @@
 	<!-- post's own thumbnail -->
 	<div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large',  array( 'class' => 'img-responsive center-block' )); ?></a></div>
 
-<<<<<<< HEAD
-<div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large',  array( 'class' => 'img-responsive center-block' )); ?></a></div>
-
-<?php else : 
-$category = get_the_category(); 
-?>
-
-<div class="entry-thumbnail">
-<a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php bloginfo('url'); ?>/wp-content/uploads/category-images/<?php echo $category[0]->category_nicename ; ?>-360x180.jpg" alt="<?php the_title(); ?>" /></a>
-</div>
-=======
 <?php else : $category = get_the_category(); ?>
 	<!-- defaukt category thumbnail -->
 	<div>
 		<a href="<?php the_permalink(); ?>"><img class="img-responsive center-block" src="<?php bloginfo('url'); ?>/wp-content/uploads/category-images/<?php echo $category[0]->category_nicename ; ?>-360x180.jpg" alt="<?php the_title(); ?>" /></a>
 	</div>
->>>>>>> origin/master
 
 <?php endif; ?>
 
