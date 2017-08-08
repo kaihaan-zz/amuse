a0a1d9ee8bb1e39a68f8a10fdc3c7c92529f0582
Kaihaan <kaihaan@gmail.com>
2017-08-08 09:59:52 +0100

    default cat images

--- wp-content/themes/amuse/content-grid.php
+++ wp-content/themes/amuse/content-grid.php
@@ -5,14 +5,14 @@
 
 <?php if ( has_post_thumbnail() ) : ?>
 
-<div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large',  array( 'class' => 'img-responsive' )); ?></a></div>
+<div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large',  array( 'class' => 'img-responsive center-block' )); ?></a></div>
 
 <?php else : 
 $category = get_the_category(); 
 ?>
 
 <div class="entry-thumbnail">
-<a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php bloginfo('url'); ?>/wp-content/uploads/category-images/<?php echo $category[0]->category_nicename ; ?>-360x180.jpg" alt="<?php the_title(); ?>" /></a>
+<a href="<?php the_permalink(); ?>"><img class="img-responsive center-block" src="<?php bloginfo('url'); ?>/wp-content/uploads/category-images/<?php echo $category[0]->category_nicename ; ?>-360x180.jpg" alt="<?php the_title(); ?>" /></a>
 </div>
 
 <?php endif; ?>
