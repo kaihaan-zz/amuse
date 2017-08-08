ebe4512932f150da827a0664ed018b9c280abd44
Kaihaan <kaihaan@gmail.com>
2017-08-08 19:34:30 +0100

    partial

--- wp-content/themes/amuse/content-grid.php
+++ wp-content/themes/amuse/content-grid.php
@@ -18,12 +18,6 @@
 <!-- Post Title -->
 <div class='grid-title'><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></div>
 
-<!-- Post Date -->
-<div class="grid-attrib"><?php echo get_the_date(); ?></div>
-
-<!-- Post Excerpt -->
-<div class="grid-excerpt"><?php the_excerpt(); ?></div>
-
 <!-- Post Category Link -->
 <div class="grid-attrib"><?php
 	$categories = get_the_category();
@@ -32,4 +26,9 @@
 		echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';   
 	}
 ?></div>
+
+<!-- Post Excerpt -->
+<div class="grid-excerpt"><?php the_excerpt(); ?></div>
+
+
 <?php // the_content(); ?>
